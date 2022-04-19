4e8a954730cc9509149f0489e3a2cba93a32ac6b
Martin Farrera <farreralara.martin@gmail.com>
2022-04-19 01:11:50 -0500

    Se agrega registro de usurio

--- server.py
+++ server.py
@@ -1,41 +1,38 @@
-from flask import Flask
-from flask import render_template, request, url_for
-
+from flask import Flask # importamos la libreria flask
+from flask import render_template, request, url_for #importamos render_templetam, request, url _for
 app = Flask(__name__)
-PORT = 5000
+PORT = 8000
 DEBUG = True
-
-# Ruta si el archivo no se encuentra
 @app.errorhandler(404)
 def not_found(error):
     return "Not Found"
 
-# Ruta de la visualización
-@app.route('/', methods=['GET'])
+@app.route('/',methods=['GET'])
 def index():
-    name ="Luis"
-    ap="Estrada"
-    return render_template('index3.html', name=name, ap= ap)
-    #return render_template('index.html')
+    #texto="""<h1></h1>"""
+    #return texto
+    #return render_template('index2.html')
+    return render_template('index3.html')
 
-@app.route('/home', methods=['POST'])
+@app.route('/home',methods=['POST'])
 def Home():
     nombre=request.form['name']
     ap=request.form['ap']
-    return render_template('home.html', nombre=nombre, app=ap)
+    return render_template('home.html',nombre=nombre,ap=ap)
 
-@app.route('/reg', methods=['GET'])
-def reg():
+@app.route('/reg',methods=['GET'])
+def reg():    
     return render_template('registro.html')
 
-@app.route('/val', methods=['POST'])
-def val():
-    nombre = ""
-    ap = ""
-    correo= ""
-    psw = ""
-    edad= ""
-    return render_template("regisexi.html")
+@app.route('/val',methods=['POST'])
+def val():    
+    name=request.form['name']
+    ap=request.form['ap']
+    tel=request.form['tel']
+    ema=request.form['ema']
+    contr=request.form['contr']
+    return render_template('regisexi.html',name=name,ap=ap,tel=tel,ema=ema,contr=contr)
 
-if __name__ == '__main__':
+
+if __name__== '__main__':
     app.run(port=PORT,debug=DEBUG)
